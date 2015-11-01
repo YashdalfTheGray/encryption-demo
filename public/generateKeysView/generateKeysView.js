ace17996e3d5d5294310ddba34c03c3b98edec77
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-10-31 20:06:29 -0500

    added a view for generating keys

--- public/generateKeysView/generateKeysView.js
+++ public/generateKeysView/generateKeysView.js
@@ -3,10 +3,45 @@
 angular.module('encryptionDemo')
 .controller('GenerateKeysViewCtrl',
     [
-        function() {
+        '$window',
+        function($window) {
             "use strict";
 
             var vm = this;
+            var randomArray = new Uint32Array(8);
+            var randomStrArray = [];
+
+            vm.numberBits = 2048;
+
+            vm.email = "test.testerson@testdomain.com";
+            vm.password = "SierraControllerAmplifierLake";
+
+            function getSalt() {
+                $window.crypto.getRandomValues(randomArray);
+                _.forEach(randomArray, function(item) {
+                    randomStrArray.push(item.toString(16));
+                });
+                return randomStrArray.join('');
+            }
+
+            vm.generate = function() {
+                var options = {
+                    numBits: vm.numberBits,
+                    userId: vm.email,
+                    passphrase: vm.password.toString() + getSalt()
+                };
+                $window.openpgp.generateKeyPair(options).then(function(keypair) {
+                    $window.localStorage.setItem('openPGP.publicKey', keypair.publicKeyArmored);
+                    $window.localStorage.setItem('openPGP.privateKey', keypair.privateKeyArmored);
+                }).catch(function(error) {
+                    console.log(error);
+                });
+            };
+
+            vm.refresh = function() {
+                vm.publicKey = $window.localStorage.getItem('openPGP.publicKey');
+                vm.privateKey = $window.localStorage.getItem('openPGP.privateKey');
+            }
         }
     ]
 );
